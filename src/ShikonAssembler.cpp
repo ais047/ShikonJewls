d7dff17db18671b789ffbc6d4fba55f5b44f470d
root <root@localhost.localdomain>
2019-05-01 19:35:45 -0700

    base64

--- src/ShikonAssembler.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Seperating out File into 4 Pieces";
-	return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include "base64.h"
+#include <iostream>
+
+int main() {
+  const std::string s =
+    "René Nyffenegger\n"
+    "http://www.renenyffenegger.ch\n"
+    "passion for data\n";
+
+  std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());
+  std::string decoded = base64_decode(encoded);
+
+  std::cout << "encoded: " << std::endl << encoded << std::endl << std::endl;
+  std::cout << "decoded: " << std::endl << decoded << std::endl;
+
+
+  // Test all possibilites of fill bytes (none, one =, two ==)
+  // References calculated with: https://www.base64encode.org/
+
+  std::string rest0_original = "abc";
+  std::string rest0_reference = "YWJj";
+
+  std::string rest0_encoded = base64_encode(reinterpret_cast<const unsigned char*>(rest0_original.c_str()),
+    rest0_original.length());
+  std::string rest0_decoded = base64_decode(rest0_encoded);
+
+  std::cout << "encoded:   " << rest0_encoded << std::endl;
+  std::cout << "reference: " << rest0_reference << std::endl;
+  std::cout << "decoded:   " << rest0_decoded << std::endl << std::endl;
+
+  return 0;
+}
+
